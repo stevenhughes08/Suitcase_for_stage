9e13f01be8d8256f0e8bf504ff9b4b7141c6957e
Steven <stevenhughes08@gmail.com>
2020-04-11 00:00:50 -0400

    updated scaffolding but DB will not build

--- suitcase/Startup.cs
+++ suitcase/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using suitcase.Data;
 
 namespace suitcase
 {
@@ -24,6 +26,7 @@ namespace suitcase
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            IServiceCollection serviceCollection = services.AddDbContext<Context>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
