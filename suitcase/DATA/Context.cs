7108200ec8075b6b793ae78b69e9f2f0d4ee9ed3
Steven <stevenhughes08@gmail.com>
2020-04-11 17:57:24 -0400

    Scaffolded the database

--- suitcase/DATA/Context.cs
+++ suitcase/DATA/Context.cs
@@ -1,22 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using suitcase.Models;
 
-
 namespace suitcase.Data
 {
     public class Context : DbContext
     {
-         Context(DbContextOptions<Context> options) : base(options) {}
-         
-         protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("Data Source=SuitcaseContext.db");
-         protected override void OnModelCreating(ModelBuilder modelBuilder) {
-             modelBuilder.Entity<ActPerformer>().HasKey(t => new { t.PerformerId, t.ActId });
-             modelBuilder.Entity<ActProp>().HasKey(t => new { t.PropId, t.ActId });
-         }
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
 
-         DbSet<Act> Acts { get; set; }
-         DbSet<Performance> Performances { get; set; }
-         DbSet<Performer> Performers { get; set; }
-         DbSet<Prop> Props { get; set; }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<ActPerformer>().HasKey(t => new { t.PerformerId, t.ActId });
+            modelBuilder.Entity<ActProp>().HasKey( t => new { t.PropId, t.ActId });
+        }
+        DbSet<Act> Acts { get; set; }
+        DbSet<Performance> Performances { get; set; }
+        DbSet<Performer> Performers { get; set; }
+        DbSet<Prop> Props {get; set; }
     }
 }
--- suitcase/Startup.cs
+++ suitcase/Startup.cs
@@ -25,8 +25,9 @@ namespace suitcase
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
-            IServiceCollection serviceCollection = services.AddDbContext<Context>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
