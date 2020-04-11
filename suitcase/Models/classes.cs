1784c88f95ed7450ee840898c6d3f4ff45e43bc6
Steven <stevenhughes08@gmail.com>
2020-04-10 21:48:41 -0400

    added models

---
+++ suitcase/Models/classes.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace suitcase.Models
+{
+    
+    public class BaseNamedEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Performer : BaseNamedEntity
+    {
+        public string HeadShotUrl { get; set; }
+        public ICollection<ActPerformer> ActPerformers { get; set; }
+    }
+
+    public class Prop : BaseNamedEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid PropId { get; set; }
+        public string StorageLocation { get; set; }
+        public ICollection<ActProp> ActProps { get; set; }
+    }
+
+     public class Act : BaseNamedEntity
+    {
+        public Guid PerformanceId { get; set; }
+        public Performance Performance { get; set; }
+        public ICollection<ActPerformer> ActPerformers { get; set; }
+        public ICollection<ActProp> ActProps { get; set; }
+        
+    }
+    public class Performance : BaseNamedEntity
+    {       
+        public ICollection<Act> Acts { get; set; }
+    }
+
+    public class ActProp
+    {
+        public Guid ActId { get; set; }
+        [ForeignKey("ActId")]
+        public Act Act { get; set; }
+        public Guid PropId { get; set; }
+        [ForeignKey("PropId")]
+        public Prop Prop { get; set; }
+    
+    }
+
+    public class ActPerformer 
+    {
+      public Guid PerformerId { get; set; }
+        [ForeignKey("PerformerId")]
+        public Performer Performer { get; set; }
+        public Guid ActId { get; set; }
+        [ForeignKey("ActId")]
+        public Act Act { get; set; }
+
+    }
+
+}
