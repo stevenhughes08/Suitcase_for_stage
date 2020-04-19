072e563b75b5ea9b3816833a9daca15139a69518
Steven <stevenhughes08@gmail.com>
2020-04-19 15:12:07 -0400

    Added HomeController update

---
+++ suitcase/Models/SetlistViewModels/SetlistGroup.cs
@@ -0,0 +1,13 @@
+namespace suitcase.Models.SetlistViewModels
+{
+    public class SetlistGroup
+    {
+        public Performance Performance { get; set; }
+        public Act Act { get; set; }
+
+        public Prop Prop { get; set; }
+
+        public Performer Performer { get; set; }
+
+    }
+}
